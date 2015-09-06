1ea0b257b74bedc91684747ed068d4b500ff7020
uenala <uenala@users.noreply.github.com>
2015-09-06 19:43:50 +0200

    Issue #1: delete accidentially checked in js and map files.

--- SpaBergaufDotCh/app/home/data/gallery.js
+++
@@ -1,9 +0,0 @@
-///<reference path='../../../typings/tsd.d.ts' />
-var Home;
-(function (Home) {
-    var Data;
-    (function (Data) {
-        'use strict';
-    })(Data = Home.Data || (Home.Data = {}));
-})(Home || (Home = {}));
-//# sourceMappingURL=gallery.js.map
