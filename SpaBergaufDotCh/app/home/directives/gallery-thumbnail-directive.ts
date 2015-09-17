60ec57f08168cd6ffaab4c129b36e14e7dad8f8e
RAeschimann <roland.aeschimann@gmail.com>
2015-09-17 18:00:09 +0200

    directive for a gallery thumbnail added (issue #14)

---
+++ SpaBergaufDotCh/app/home/directives/gallery-thumbnail-directive.ts
@@ -0,0 +1,50 @@
+///<reference path='../../../typings/tsd.d.ts' />
+module GalleryThumbnail {
+  'use strict';
+
+  /**
+  * @ngdoc directive
+  * @name home.directive:galleryThumbnail
+  * @restrict E
+  * @element
+  *
+  * @description
+  *
+  * @example
+    <example module="home">
+      <file name="index.html">
+        <gallery-thumbnail></gallery-thumbnail>
+      </file>
+    </example>
+  *
+  */
+  angular
+    .module('home')
+    .directive('galleryThumbnail', galleryThumbnail);
+
+  function galleryThumbnail(): ng.IDirective {
+
+    return {
+      restrict: 'E',
+      scope: {
+      },
+      templateUrl: '/home/views/gallery-thumbnail.tpl.html',
+      replace: false,
+      controllerAs: 'galleryThumbnail',
+      controller: function () {
+        var vm = this;
+        vm.name = 'galleryThumbnail';
+      },
+      link: function (scope: ng.IScope, element: JQuery, attrs: any) {
+        /*jshint unused:false */
+        /*eslint "no-unused-vars": [2, {"args": "none"}]*/
+
+        attrs.$observe('gallery', function(gallery) {
+          scope.galleryObj = scope.$eval(gallery);
+
+        });
+
+      }
+    };
+  }
+}
