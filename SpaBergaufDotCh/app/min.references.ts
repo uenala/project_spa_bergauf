d6a30b4795614076f55ce4a59823c8f5c4e97db5
uenala <uenala@users.noreply.github.com>
2015-09-30 18:58:56 +0200

    add a minimal dependency file for type-script

---
+++ SpaBergaufDotCh/app/min.references.ts
@@ -0,0 +1,18 @@
+/// <reference path="../typings/tsd.d.ts"/>
+
+///<reference path='home/services/authentication-service.ts' />
+///<reference path='home/services/flash-service.ts' />
+///<reference path='home/services/repository-service.ts' />
+///<reference path='home/services/user-local-storage-service.ts' />
+///<reference path='home/services/user-service.ts' />
+
+///<reference path='home/data/gallery.ts' />
+///<reference path='home/data/image.ts' />
+
+///<reference path='home/constants/the-activities-constant.ts' />
+///<reference path='home/constants/the-countries-constant.ts' />
+
+
+
+
+
