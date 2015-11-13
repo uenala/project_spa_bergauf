9e831122ebb9cb8d92ffc9f54a48c096606f8733
RAeschimann <roland.aeschimann@gmail.com>
2015-11-13 18:01:57 +0100

    unit tests for convert-id-to-property filter (WIP)

--- SpaBergaufDotCh/app/home/filters/convert-id-to-property-filter_test.ts
+++ SpaBergaufDotCh/app/home/filters/convert-id-to-property-filter_test.ts
@@ -1,13 +1,69 @@
-///<reference path='../../../typings/tsd.d.ts' />
+///<reference path='../../min.references.ts' />
 
 /* global describe, beforeEach, it, expect, inject, module */
 'use strict';
 
 describe('convertIdToProperty', function () {
+
   beforeEach(angular.mock.module('home'));
 
-//  it('should filter our numbers not greater than 3', inject(function ($filter) {
-//    expect($filter('convertIdToProperty')([1, 2, 3, 4])).toEqual([4]);
-//  }));
+  it("should initialize correctly", inject(function ($filter) {
+    expect($filter('convertIdToProperty')).toBeDefined();
+  }));
+
+  it('should return activity name', inject(function ($filter) {
+    // ToDo: inject tags with beforeEach
+    var tags = [
+      [{"name": "Alle Aktivitäten","id": ""}, {"name": "Skitour","id": "skitour"}],
+      [{"name": "Alle Länder","code": "","id": ""}, {"name": "Schweiz","code": "CH","id": "schweiz"}],
+      [{"name": "Alborz","id": "alborz"}, {"name": "Altiplano","id": "altiplano"}]
+    ];
+    expect($filter('convertIdToProperty')('skitour', tags, 'activities', 'name')).toBe("Skitour");
+  }));
+
+  it('should return activity default property', inject(function ($filter) {
+    var tags = [
+      [{"name": "Alle Aktivitäten","id": ""}, {"name": "Skitour","id": "skitour"}],
+      [{"name": "Alle Länder","code": "","id": ""}, {"name": "Schweiz","code": "CH","id": "schweiz"}],
+      [{"name": "Alborz","id": "alborz"}, {"name": "Altiplano","id": "altiplano"}]
+    ];
+    expect($filter('convertIdToProperty')('skitour', tags, 'activities')).toBe("Skitour");
+  }));
+
+  it('should return activity default label', inject(function ($filter) {
+    var tags = [
+      [{"name": "Alle Aktivitäten","id": ""}, {"name": "Skitour","id": "skitour"}],
+      [{"name": "Alle Länder","code": "","id": ""}, {"name": "Schweiz","code": "CH","id": "schweiz"}],
+      [{"name": "Alborz","id": "alborz"}, {"name": "Altiplano","id": "altiplano"}]
+    ];
+    expect($filter('convertIdToProperty')('', tags, 'activities')).toBe("Alle Aktivitäten");
+  }));
+
+  it('should return country name', inject(function ($filter) {
+    var tags = [
+      [{"name": "Alle Aktivitäten","id": ""}, {"name": "Skitour","id": "skitour"}],
+      [{"name": "Alle Länder","code": "","id": ""}, {"name": "Schweiz","code": "CH","id": "schweiz"}],
+      [{"name": "Alborz","id": "alborz"}, {"name": "Altiplano","id": "altiplano"}]
+    ];
+    expect($filter('convertIdToProperty')('schweiz', tags, 'countries', 'name')).toBe("Schweiz");
+  }));
+
+  it('should return country code', inject(function ($filter) {
+    var tags = [
+      [{"name": "Alle Aktivitäten","id": ""}, {"name": "Skitour","id": "skitour"}],
+      [{"name": "Alle Länder","code": "","id": ""}, {"name": "Schweiz","code": "CH","id": "schweiz"}],
+      [{"name": "Alborz","id": "alborz"}, {"name": "Altiplano","id": "altiplano"}]
+    ];
+    expect($filter('convertIdToProperty')('schweiz', tags, 'countries', 'code')).toBe("CH");
+  }));
+
+  it('should return region name', inject(function ($filter) {
+    var tags = [
+      [{"name": "Alle Aktivitäten","id": ""}, {"name": "Skitour","id": "skitour"}],
+      [{"name": "Alle Länder","code": "","id": ""}, {"name": "Schweiz","code": "CH","id": "schweiz"}],
+      [{"name": "Alborz","id": "alborz"}, {"name": "Altiplano","id": "altiplano"}]
+    ];
+    expect($filter('convertIdToProperty')('altiplano', tags, 'regions', 'name')).toBe("Altiplano");
+  }));
 
 });
